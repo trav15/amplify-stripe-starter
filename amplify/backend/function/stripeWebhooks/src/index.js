a0bc94a2310dc5bea5d840d1979feee9722a62c4
trav15 <travistokuyama@gmail.com>
2022-12-22 23:18:28 +0000

    Update Stripe webhook and push to Amplify

--- amplify/backend/function/stripeWebhooks/src/index.js
+++ amplify/backend/function/stripeWebhooks/src/index.js
@@ -1,4 +1,22 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
 
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["STRIPE_SECRET_KEY","STRIPE_WEBHOOK_SECRET"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_PRODUCTIMAGES_BUCKETNAME
+Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
